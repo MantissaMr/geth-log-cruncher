1932a03a569970c94c7104a62d23690092fcce27
Alameen Ibrahim <alamiin.sanusi@gmail.com>
2025-11-03 21:21:53 +0100

    feat: Add initial CLI structure and argument parsing with clap

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+
+struct Cli {
+    /// The path to the log file to be processed.
+    log_file_path: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli_args = Cli::parse();
+    println!("Processing log file at path: {}", cli_args.log_file_path);
 }
